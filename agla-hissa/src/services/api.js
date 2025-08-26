2f254b5c4026683e9fb31737cdfd51047145b53a
Devansh <devansh28112004@gmail.com>
2025-08-26 10:08:26 +0530

    refactor: Move API_BASE_URL to a separate url.js file for better configuration management

--- agla-hissa/src/services/api.js
+++ agla-hissa/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
-// const API_BASE_URL = 'http://127.0.0.1:8000';
-const API_BASE_URL = 'https://api.dev-ansh.xyz';
+import { API_BASE_URL } from '../url';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
