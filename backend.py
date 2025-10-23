4d91efc448db355563ddb45cee5605a7822e7ada
Devansh <devansh28112004@gmail.com>
2025-10-24 04:43:18 +0530

    minor config change

--- backend.py
+++ backend.py
@@ -3,8 +3,8 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",    
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=3000,
         log_level="info",
-        reload=True       
+        reload=False       
     )
